79e2b25c8092a6e0063a7f8f2d9d5946d8fe88ef
unknown <molinahuel44@gmail.com>
2025-01-09 23:36:41 -0300

    converting no tsv to tsv;

--- data_analysis.js
+++ data_analysis.js
@@ -1,8 +1,36 @@
+//the thing is that the file is not tab separated, it has multiple spaces
 const path = 'data/tohoku_2011/max_acc_list.txt'
 fetch(path)
     .then(res => res.text()
     )
     .then(content => {
+        clean_data = delete_header(content);
+        makin_dataset(clean_data);
         console.log(content);
     })
     .catch(err => console.log(err));
+
+
+function delete_header(raw_data) {
+    return data;
+}
+function makin_dataset(data) {
+    let places = [];
+    let buffer = '';
+    let count_spaces = 0;
+    let len = data.length;
+    let row = '';
+    for (let i=0; i<len; i++) {
+        buffer = buffer + i;
+        if (i == ' ') {
+            row = row + buffer;
+            if(count_spaces == 1) {
+                buffer = buffer + '\t';
+            } else {
+                count_spaces++;
+            }
+        } else {
+            count_spaces = 0;
+        }
+    }
+}
